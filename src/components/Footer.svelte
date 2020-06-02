ee2633d4fca25a70d0208d912f2b3c3ebd7d7f90
zubair <zubairzia0@gmail.com>
2020-06-02 01:47:39 -0400

    initializing

---
+++ src/components/Footer.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<style>
+  footer {
+    padding: 40px;
+    text-align: center;
+  }
+
+  .copyright {
+    color: #aaa;
+    font-size: 14px;
+    display: inline-block;
+    padding: 20px;
+    border-top: 1px solid #ddd;
+  }
+</style>
+
+<footer>
+  <div class="copyright">Copyright 2020</div>
+</footer>
---
+++ src/components/Header.svelte
@@ -0,0 +1,24 @@
+<script>
+
+</script>
+
+<style>
+  header {
+    background: #f7f7f7;
+    padding: 20px;
+  }
+  h1 {
+    margin: 0;
+    padding: center;
+  }
+  img {
+    max-width: 180px;
+  }
+</style>
+
+<header>
+  <h1>
+    <img src="/img/Logosmall.png" alt="Poll Logo" />
+
+  </h1>
+</header>
